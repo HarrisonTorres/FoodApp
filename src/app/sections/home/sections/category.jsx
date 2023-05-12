ecaa9675ee07b63e4ff8c50160f392cd9abecd49
Harrison Torres <harri.torres12@gmail.com>
2023-05-12 00:22:30 -0300

    Adicionando itens ao carrinho mas com bug de remover 4

--- src/app/sections/home/sections/category.jsx
+++ src/app/sections/home/sections/category.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ListFormat } from "typescript";
 
 import { dados_lanches } from "/database/api_fake";
 
@@ -22,7 +21,7 @@ export default function Category() {
         <ul className="flex flex-row gap-2 overflow-scroll pb-3">
 
           {dados_lanches.map((item) =>(
-              <li className="py-2 px-3 bg-orange-400 shadow-md shadow-orange-300 rounded-3xl flex flex-row item-center justify-center gap-2 cursor-pointer">
+              <li kay={item.id} className="py-2 px-3 bg-orange-400 shadow-md shadow-orange-300 rounded-3xl flex flex-row item-center justify-center gap-2 cursor-pointer">
                 <div className="w-7 h-7 rounded-full bg-white"></div>
                 <p className="text-white text-xs font-medium m-auto">
                 {item.category}
